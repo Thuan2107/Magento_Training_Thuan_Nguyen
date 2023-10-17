74e14fb2ce0aa348977d005e137cf27a8bdb8ccf
Nguyen Minh Thuan <mtnguyen2107@gmail.com>
2023-10-17 14:03:37 +0700

    complete home page

--- app/design/frontend/my_vendor/my _theme/Magento_Theme/web/js/main.js
+++ app/design/frontend/my_vendor/my _theme/Magento_Theme/web/js/main.js
@@ -13,8 +13,32 @@ require(['jquery', 'slick'], function ($) {
 
 require(['jquery', 'slick'], function ($) {
     $(document).ready(function () {
-        $('.dealsofdaywrapper').slick({
+        $('.deals_of_day_wrapper').slick({
             slidesToShow: 5,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 1500,
+            arrows: true, // Hiển thị nút next và prev
+        });
+    });
+});
+
+require(['jquery', 'slick'], function ($) {
+    $(document).ready(function () {
+        $('.best_seller_wrapper_item').slick({
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 1500,
+            arrows: true, // Hiển thị nút next và prev
+        });
+    });
+});
+
+require(['jquery', 'slick'], function ($) {
+    $(document).ready(function () {
+        $('.news_wrapper_item').slick({
+            slidesToShow: 4,
             slidesToScroll: 1,
             autoplay: true,
             autoplaySpeed: 1500,
